cd4877a4aa5be7daeef788c6df6f15b488e876a1
Bruno Lima <brunolima280103@gmail.com>
2023-05-13 10:37:53 -0300

    implementando sequelize + tabela de usuário

---
+++ Semana-4/api/domains/usuarios/models/usuario.js
@@ -0,0 +1,30 @@
+const sequelize = require('../../../database/index');
+const {DataTypes} = require('sequelize');
+
+const usuario = sequelize.define('Usuario', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    nome:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    senha:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+
+usuario.sync({alter: false, force: false})
+    .then(() => {
+        console.log('Tabela de usuários foi (re)criada');
+    }).catch((err) => console.log(err));
+
+module.exports = usuario;
